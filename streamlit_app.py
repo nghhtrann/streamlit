a96a2ef6690cccd6645b86f7a8a388f06e466200
Tran Nguyen <haihuongtran.ng@gmail.com>
2024-12-18 11:15:22 +0100

    initial streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.header("Introduction", divider = True)
+
+st.subheader("Welcome!")
+
+st.write("Hello c'est Séraphine!")
+
+st.text("This course is short intro to DevOps")
